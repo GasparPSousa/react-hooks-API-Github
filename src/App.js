d838faaa12d052da6d825eb8f0e369c10b9f3816
GasparPSousa <gasparufrj@gmail.com>
2021-12-02 23:32:50 -0300

    Utizando UseState de forma estática

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+   const [repositories, setRepositories] = useState([
+     { id: 1, name: "repo-1" },
+     { id: 2, name: "repo-2" },
+     { id: 3, name: "repo-3" },
+   ])
+
+   const handleAddRepository = () => {
+     setRepositories ([...repositories, { id: Math.random(), name: 'Novo repo' }
+     ]);
+    }
+
   return (
     <>
+      <ul>
+        { repositories.map(repo => (<li key={repo.id}> {repo.name} </li>))}
+      </ul>
+      <button onClick={handleAddRepository}> Adicionar Repositório </button>
     </>
   );
 }
