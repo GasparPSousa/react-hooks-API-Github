23102f107f416630136071402be93ed12d418950
GasparPSousa <gasparufrj@gmail.com>
2021-12-02 23:58:17 -0300

    Puxando os dados da API do Github e usando o useEffet como um componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
 
 function App() {
+  
+   const [repositories, setRepositories] = useState([])
 
-   const [repositories, setRepositories] = useState([
-     { id: 1, name: "repo-1" },
-     { id: 2, name: "repo-2" },
-     { id: 3, name: "repo-3" },
-   ])
+   // Carregando uma informação assim que o componente for montado em tela(componentDidMount)
+   useEffect(async () => {
 
-   const handleAddRepository = () => {
-     setRepositories ([...repositories, { id: Math.random(), name: 'Novo repo' }
-     ]);
-    }
+      // puxando os dados da api do github
+      const response = await fetch('https://api.github.com/users/gasparpsousa/repos');
+      const data = await response.json();
+
+      // carregando os repositórios com dados da minha api github
+      setRepositories(data);
+
+   }, []) // A função setRepositories será executada apenas uma vez nesse cenário, já que o segundo argumento é um array vazio, ou seja, não está passando nenhuma dependência para esse effect, não está passando nenhuma comparação. 
 
   return (
     <>
       <ul>
         { repositories.map(repo => (<li key={repo.id}> {repo.name} </li>))}
       </ul>
-      <button onClick={handleAddRepository}> Adicionar Repositório </button>
     </>
   );
 }
